6562462f508b9a5f887670d2cd0a8dacf202d6c3
Eric Ni <ericni2000@gmail.com>
2021-06-15 15:20:53 -0700

    Changed font styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,8 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hello, my name is Eric.</h1>
-      <h2>I'm an aspiring software engineer and student</h2>
+      <h1 className={styles.header1}>Hello, my name is Eric.</h1>
+      <h2 className={styles.header2}>I'm an aspiring software engineer and student</h2>
     </div>
   );
 }

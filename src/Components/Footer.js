ef217dbb83282240b9d4213b89e45f2acc798039
Eric Ni <ericni2000@gmail.com>
2021-06-16 11:57:35 -0700

    Fixed some responsiveness bugs and added resume to website

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import { AiFillLinkedin, AiFillGithub, AiFillMail } from "react-icons/ai";
+import { AiFillLinkedin, AiFillGithub, AiFillMail, AiFillIdcard} from "react-icons/ai";
+import resume from "./files/Resume.pdf";
 
 export default function Footer() {
   return (
-    <div className={styles.container}>
-      <a onClick={() => window.open("https://www.linkedin.com/in/ecni")}><AiFillLinkedin className={styles.icon}/></a>
-      <a onClick={() => window.open("https://www.github.com/ecni19")}><AiFillGithub className={styles.icon}/></a>
-      <a onClick={() => window.open('mailto:ericni2000@gmail.com')}><AiFillMail className={styles.icon}/></a>
+    <div>
+      <div className={styles.container}>
+        <a onClick={() => window.open("https://www.linkedin.com/in/ecni")}><AiFillLinkedin className={styles.icon}/></a>
+        <a onClick={() => window.open("https://www.github.com/ecni19")}><AiFillGithub className={styles.icon}/></a>
+        <a onClick={() => window.open('mailto:ericni2000@gmail.com')}><AiFillMail className={styles.icon}/></a>
+        <a onClick={() => window.open(resume)}><AiFillIdcard className={styles.icon}/></a>
+      </div>
+      <p className={styles.copyright}>&#169; Eric Ni 2021</p>
     </div>
   )
 }
